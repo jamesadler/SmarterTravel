85dcdef98390acdb2050ddafad8f7bf295120ca1
jamesadler <jfa@jamesfadler.com>
2016-09-08 15:49:44 -0400

    Initial Upload
    Initial file creation, included task 2 info

---
+++ task2.js
@@ -0,0 +1,9 @@
+/*
+ * Task 2 
+ * At the top of the page there is a tagline that contains something similar to 
+ * "Searching 384 San Francisco Hotels". Modify this string to say 
+ * "Showing XX out of 384 San Francisco Hotels" where XX is the number of hotels 
+ * currently listed on the page. Note that this page uses 'infinite scrolling',
+ * so as you scroll down more hotels are loaded in via AJAX. The number of hotels 
+ * in the tagline should be updated to reflect this as the user scrolls. 
+ */
