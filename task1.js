585a5146692b63986d57863555633e797c71920b
jamesadler <jfa@jamesfadler.com>
2016-09-08 15:02:29 -0400

    Initial uplaod
    Initial file creation, included task 1 info

---
+++ task1.js
@@ -0,0 +1,6 @@
+/*
+ * Task 1 
+ * Write JavaScript code that fetches the destination name, check in date, and 
+ * check out date of the page the user is on and prints them to the console. 
+ * You can find these values in the search form near the top of the page. 
+*/
