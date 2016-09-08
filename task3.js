bdab63c5b7ea0c1a9c6b1601b21b00340b57b915
jamesadler <jfa@jamesfadler.com>
2016-09-08 19:25:22 -0400

    Initial Upload
    Initial file creation, including task 3 info.

---
+++ task3.js
@@ -0,0 +1,13 @@
+/* 
+ * Task 3 
+ * Each hotel in the listing has a 'Select' button that when clicked takes the 
+ * user to a detail page about that specific hotel. Write JavaScript code that 
+ * changes the functionality of this button to instead do 2 things 
+ * 1) Opens the hotel’s detail page in a new browser window instead of reusing 
+ *    the same one as it currently does 
+ * 2) Add the name of the hotel chosen to a new list somewhere on the hotel 
+ *    listing page that says “Selected hotels”. You can choose where this list 
+ *    is displayed and how it is styled, but it must be visible on the hotel 
+ *    listings page. Note that If the user chooses the same hotel twice, the 
+ *    name should not be duplicated in the list of 'Selected hotels'. 
+ */
