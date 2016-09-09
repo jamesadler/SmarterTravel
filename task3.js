dfe52224de10d1644e0f91478b6297ee69eeee0f
jamesadler <jfa@jamesfadler.com>
2016-09-08 22:19:57 -0400

    Sub-task 2 - style
    Fixed styling of the "Selected Hotels" module

--- task3.js
+++ task3.js
@@ -12,25 +12,43 @@
  *    name should not be duplicated in the list of 'Selected hotels'. 
  */
 $(document).ready(function(){
+	// clicking the "select" button for a hotel opens the details page in a new window
 	$("a.prominent_button.do_show_rates").each(function(){
 		$(this).attr('target','_blank');
 	});
 
+	// creates the module that will hold selected hotels
 	$("#curated_geo").after('<div class="module" id="selectedHotels"></div>');
-	$("#selectedHotels").append('<div class="container_title">Selected Hotels</div>');
+
+	$("#selectedHotels").append('<div class="container_title">Selected Hotels</div>').css('padding','5px 0');
 	$("#selectedHotels > .container_title").css({
 		'padding' : '5px 14px',
 		'font-weight' : 'bold',
 		'font-size' : '16px'
 	});
-	$("#selectedHotels").append('<div class="row" id="hotelNames"></div>');
-	$("#hotelNames").append('<div class="content"><ol id="selectedHotelsList"></ol></div>');
+	$("#selectedHotels").append('<div id="hotelNames" class="row clearfix"></div>');
 
+	// Copied the style of the "Refine Results" module
+ 	$("#hotelNames").css('background','-webkit-gradient(linear,left top,left bottom,from(#ecf7fb),to(#fff))');
+ 	$("#hotelNames").css('background','-moz-linear-gradient(center top,#ecf7fb 0,#fff 100%)');
+ 	$("#hotelNames").css('background','-moz-gradient(center top,#ecf7fb 0,#fff 100%)');
+ 	$("#hotelNames").css('background','linear-gradient(#ecf7fb,#fff)');
+ 	$("#hotelNames").css('background','-ms-linear-gradient(#ecf7fb,#fff)');
+ 	$("#hotelNames").css('border-top','1px #ccc solid');
+ 	$("#hotelNames").css('padding-bottom','13px');
+
+	$("#hotelNames").append('<ul id="selectedHotelsList"></ul>');
+	$("#selectedHotelsList").css({
+		'list-style-type' : 'square',
+		'margin-left' : '31px'
+	});
+
+	// when the user selects a hotel, the name is atted to the "Selected Hotel" module
 	$("a.prominent_button.do_show_rates").click(function(){
 		var hotelName = $(this).closest(":has( .hotel_detail_link)").find(".hotel_detail_link").html();
 
 		if($("#selectedHotelsList :contains('"+hotelName+"')").length == 0){
-			$("#selectedHotelsList").append("<li>"+hotelName+"</li>");
+			$("#selectedHotelsList").append("<li class='hotelListItem'>"+hotelName+"</li>");
 		}
 	});
 });
