4845decdc6c2e27a4199e6c0c11c469a3e95687e
jamesadler <jfa@jamesfadler.com>
2016-09-08 22:27:44 -0400

    Cleaning up code
    Added some more comments and renamed a variable

--- task3.js
+++ task3.js
@@ -11,6 +11,7 @@
  *    listings page. Note that If the user chooses the same hotel twice, the 
  *    name should not be duplicated in the list of 'Selected hotels'. 
  */
+ 
 $(document).ready(function(){
 	// clicking the "select" button for a hotel opens the details page in a new window
 	$("a.prominent_button.do_show_rates").each(function(){
@@ -20,6 +21,7 @@ $(document).ready(function(){
 	// creates the module that will hold selected hotels
 	$("#curated_geo").after('<div class="module" id="selectedHotels"></div>');
 
+	// Adds title to the "Selected Hotels" module
 	$("#selectedHotels").append('<div class="container_title">Selected Hotels</div>').css('padding','5px 0');
 	$("#selectedHotels > .container_title").css({
 		'padding' : '5px 14px',
@@ -36,19 +38,21 @@ $(document).ready(function(){
  	$("#hotelNames").css('background','-ms-linear-gradient(#ecf7fb,#fff)');
  	$("#hotelNames").css('border-top','1px #ccc solid');
  	$("#hotelNames").css('padding-bottom','13px');
-
-	$("#hotelNames").append('<ul id="selectedHotelsList"></ul>');
-	$("#selectedHotelsList").css({
+	
+	// Creates the list that will hold selected hotels
+	$("#hotelNames").append('<ul id="hotelNamesList"></ul>');
+	$("#hotelNamesList").css({
 		'list-style-type' : 'square',
 		'margin-left' : '31px'
 	});
 
 	// when the user selects a hotel, the name is atted to the "Selected Hotel" module
 	$("a.prominent_button.do_show_rates").click(function(){
+		// Finds the hotel name
 		var hotelName = $(this).closest(":has( .hotel_detail_link)").find(".hotel_detail_link").html();
-
-		if($("#selectedHotelsList :contains('"+hotelName+"')").length == 0){
-			$("#selectedHotelsList").append("<li class='hotelListItem'>"+hotelName+"</li>");
+		// Checks if the hotel has already beeen added to the list
+		if($("#hotelNamesList :contains('"+hotelName+"')").length == 0){
+			$("#hotelNamesList").append("<li class='hotelListItem'>"+hotelName+"</li>");
 		}
 	});
 });
